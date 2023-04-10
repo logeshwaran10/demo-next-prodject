ff18b554581c616d10cff0fa4ec57858b9b967d3
Logeshwaran <logeshwaran@mallow-tech.com>
2023-04-10 23:13:36 +0530

    Removed unwanted code

--- src/pages/productsList/[productId].js
+++ src/pages/productsList/[productId].js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 
 const ProductId = (props) => {
     const { data } = props;
-console.log('ProductId', props)
     return (
         <div>
             <p>Product name:  {data?.title}</p>
@@ -13,8 +12,6 @@ console.log('ProductId', props)
                 alt="Picture of the author"
                 width={500}
                 height={500}
-                blurDataURL={data.image} //automatically provided
-                placeholder="blur" // Optional blur-up while loading
             />
         </div>
     );
@@ -28,24 +25,21 @@ export async function getStaticPaths() {
         params: {
             productId: `${post.id}` // Params.productId key is to be used in getStaticProps()
         },
-    }))
-
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
+    }));
     return {
         paths,
         fallback: false
     }
 }
-export async function getStaticProps(context) {
-    const { params } = context;
+export async function getStaticProps(props) {
+    const { params } = props;
     const response =  await fetch(`https://fakestoreapi.com/products/${params?.productId}`).then(response => response);
     const products = await response.json()
 
     return {
         props: {
-            data: products
-        }, // will be passed to the page component as props
+            data: products // will be passed to the page component as props
+        },
     }
 }
 
